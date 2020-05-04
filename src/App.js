4e5f68702a416a208f939217f12f4f20871c3be0
Nandana <nandanasreeraj@gmail.com>
2020-05-04 18:40:20 +0530

    header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import LandingContainer from "./Components/LandingContainer";
 import AboutUs from "./Components/AboutUs";
 import Footer from "./Footer";
 import HowContainer from "./Components/HowContainer";
+import Header  from "./Components/Header"
 
 export default function App() {
   return (
     <div className="App">
+      <Header/>
       <WelcomeComponent />
       <LandingContainer />
 
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles.css";
+export default function Header() {
+  return (
+      <div class="header">
+    <div className="logo">
+        <a href="../public/index.html">OPEN<span>&lt;SOURCE&gt;</span>CODE</a>
+        <a href="javascript:void(0);" className="icon" onclick="myFunction()">
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
+      </div>
+    
+    
+  );
+}
