02b9b3a771bc7947b4490a89d7377a1cbebf8f86
alizahir23 <alizahiruddin@gmail.com>
2020-05-04 19:57:47 +0530

    fixed about section

--- src/Footer.js
+++ src/Footer.js
@@ -7,20 +7,19 @@ export default function Footer() {
   return (
     <div className="Footer1">
 
-<h1>REACH OUT TO US ON.</h1>
+      <h1>REACH OUT TO US ON.</h1>
       <div className="reach-links">
 
-      <a href="https://github.com/OpenSouceCode" >
-            <img alt="footer-github.png" src={require('./png/footer-github.png')} />
+        <a href="https://github.com/OpenSouceCode" >
+          <img alt="footer-github.png" src={require('./png/footer-github.png')} />
         </a>
         <a href="https://join.slack.com/t/opensourcecode/shared_invite/zt-dcirfg6f-IYydr1qUsAAYaq_ZA48jmg">
           <img alt="footer-slack.png" src={require('./png/footer-slack.png')} />
-          </a>
-        
+        </a>
+
         <a href="https://www.linkedin.com/company/opensourcecode"> <img alt="footer-linkedin.png" src={require('./png/footer-linkedin.png')} />
-        </a>      
+        </a>
       </div>
-   
     </div>
   );
 }
